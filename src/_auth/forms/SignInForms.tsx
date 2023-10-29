544b53d8774144a251b842f0580cb36a0c513f03
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-29 19:28:38 +0545

    basic folder structure

---
+++ src/_auth/forms/SignInForms.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignInForms = () => {
+  return (
+    <div>SignInForms</div>
+  )
+}
+
+export default SignInForms
---
+++ src/_auth/forms/SignUpForms.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUpForms = () => {
+  return (
+    <div>SignUpForms</div>
+  )
+}
+
+export default SignUpForms
