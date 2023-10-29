6fb16c7a3642123a9e89098d476ed1a769c13d1b
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-29 23:17:37 +0545

    appwrite config done

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,16 @@
+import {Client, Databases, Storage, Account, Avatars} from 'appwrite';
+
+export const appwriteConfig = {
+    projectId : import.meta.env.VITE_APPWIRTE_PROJECT_ID,
+    url : import.meta.env.VITE_APPWRITE_ENDPOINT
+}
+
+export const client = new Client();
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
+
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const account = new Account(client);
+export const avatars = new Avatars(client);
