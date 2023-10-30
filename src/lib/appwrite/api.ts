fba5b5149144fdeb723ff93dd54d7462f4efb314
saagarbhujel <saagarbhujel22@gmail.com>
2023-10-30 14:29:06 +0545

    api to save user to db redy

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,19 +1,58 @@
-import {ID} from 'appwrite';
-import { account } from './config';
-import { INewUser } from '@/types';
+import { ID } from "appwrite";
+import { account, appwriteConfig, avatars, databases } from "./config";
+import { INewUser } from "@/types";
 
+// ============================================================
+// AUTH
+// ============================================================
+
+// ============================== SIGN UP
 export const createNewUser = async (user: INewUser) => {
-    try {
-        const newAccount = await account.create(
-            ID.unique(),
-            user.email,
-            user.password,
-            user.name,
-        )
-        return newAccount;
-    } catch (error) {
-        console.log(error);
-        return error;
-        
-    }
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
+    if (!newAccount) throw new Error("Could not create new user");
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      email: newAccount.email,
+      name: newAccount.name,
+      imageUrl: avatarUrl,
+      username: user.username,
+    });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 };
+
+// ============================== SAVE USER TO DB
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
