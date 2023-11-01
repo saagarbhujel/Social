7cd11f341b371ed47b47d747126f0da6852db5c1
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-01 17:39:54 +0545

    signout function done

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -97,3 +97,16 @@ export async function getCurrentUser() {
     return null;
   }
 }
+
+
+// ============================== SIGN OUT
+
+export const signOutAccount = async () => {
+ try {
+  const session = await account.deleteSession("current");
+  
+  return session;
+ } catch (error) {
+  console.log(error);
+ }
+}
