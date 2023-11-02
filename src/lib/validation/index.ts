9ff26cb2f1fd9df9a679fe24d1d8026b74278c7e
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-02 23:29:21 +0545

    validation for posts form added

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -13,3 +13,11 @@ export const SignUpValidation = z.object({
     email: z.string().email(),
     password: z.string().min(8, {message: " Password must be at least 8 characters."}),
   })
+
+  export const PostValidation = z.object({
+    caption: z.string().min(5, {message: "Caption must be at least 5 characters."}).max(2200, {message: "Caption must be less than 2200 characters."}),
+    file: z.custom<File[]>(),
+    location: z.string().min(1, {message: "Location must be at least 1 character."}).max(1000, {message: "Location must be less than 1000 characters."}),
+    tags: z.string()
+
+  })
