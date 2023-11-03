1a59f19aa4b11a8df75470bf5aea6fb51d03d2ee
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-04 02:27:15 +0545

    data populated to form

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import PostForm from '@/components/forms/PostForm'
+import Loader from '@/components/shared/Loader';
+import { useGetPostById } from '@/lib/react-query/queriesAndMutations'
+import { useParams } from 'react-router-dom'
+
 
 const EditPost = () => {
+
+  const {id} = useParams();
+
+  const {data: post, isPending } = useGetPostById(id || "");
+
+  if(isPending) return (
+    <div className='flex-center w-full h-full'>
+      <Loader/>
+    </div>
+  )
   return (
-    <div>EditPost</div>
+    <div className='flex flex-1'>
+      <div className='common-container'>
+        <div className=' max-w-5xl flex-start gap-3 justify-start w-full'>
+          <img
+           src="/assets/icons/add-post.svg"
+           alt="add"
+           width={36}
+           height={36}
+           />
+
+          <h3 className='h3-bold md:h2-bold text-left w-full'>Create Post</h3>
+        </div>
+
+        {
+          isPending ? (
+            <Loader/>
+          ) : (
+            <PostForm action="Update" post={post} />
+          
+          )
+        }
+      </div>
+    </div>
   )
 }
 
