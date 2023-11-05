25235d31c5441c6916086908cf7f1d4c26b655f1
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-05 22:50:26 +0545

    date format refactored

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(date: string): string {
-  const parsedDate = new Date(date);
+export function formatDate(date?: string): string  {
+  const parsedDate = new Date(date? date : "");
   const currentDate = new Date();
 
   const timeDifference = currentDate.getTime() - parsedDate.getTime();
@@ -14,30 +14,26 @@ export function formatDate(date: string): string {
   if (timeDifference < 60 * 1000) {
     return "Just now";
   } else if (timeDifference < 1000 * 60 * 60) {
-    const minutesAgo = Math.floor(timeDifference / (1000 * 60));
-
-    return `${minutesAgo} minute${minutesAgo > 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 1000 * 60 * 60 * 60) {
-    const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
-
-    return `${hoursAgo} hour${hoursAgo > 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 1000 * 60 * 60 * 60 * 24) {
-    const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 60));
-
-    return `${daysAgo} day${daysAgo > 1 ? "s" : ""} ago`;
-  } else if (timeDifference < 1000 * 60 * 60 * 60 * 24 * 7) {
-    const weeksAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 60 * 24));
-
-    return `${weeksAgo} week${weeksAgo > 1 ? "s" : ""} ago`;
-  } else if (timeDifference > 1000 * 60 * 60 * 60 * 24 * 30) {
-    const monthsAgo = Math.floor(
-      timeDifference / (1000 * 60 * 60 * 60 * 24 * 7)
-    );
-
-    return `${monthsAgo} month${monthsAgo > 1 ? "s" : ""} ago`;
-  } else {
-    return "year ago";
-  }
+       const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+       return `${minutesAgo} minute${minutesAgo > 1 ? "s" : ""} ago`;
+     } else if(timeDifference < 1000 * 60 * 60 * 24) {
+        const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
+        return `${hoursAgo} hour${hoursAgo > 1 ? "s" : ""} ago`;
+     } else if(timeDifference < 1000 * 60 * 60 * 24 * 7) {
+        const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        return `${daysAgo} day${daysAgo > 1 ? "s" : ""} ago`;
+     } else if( timeDifference < 1000 * 60 * 60 * 24 * 30) {
+        const weeksAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 7));
+        return `${weeksAgo} week${weeksAgo > 1 ? "s" : ""} ago`;
+     } else if(timeDifference < 1000 * 60 * 60 * 24 * 30 * 12) {
+        const monthsAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30));
+        return `${monthsAgo} month${monthsAgo > 1 ? "s" : ""} ago`;
+     } else if(timeDifference > 1000 * 60 * 60 * 24 * 30 * 12) {
+        const yearsAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30 * 12));
+        return `${yearsAgo} year${yearsAgo > 1 ? "s" : ""} ago`;
+     } else {
+        return "Just now";
+     }
 }
 
 export const checkIsLiked = (likesList: string[], userId: string) => {
