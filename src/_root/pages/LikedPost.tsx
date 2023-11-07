525a5e0626b6b7bb6c19baa7c8e1dc45e19f3c97
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-07 23:03:38 +0545

    liked posts are fetched to profile

--- src/_root/pages/LikedPost.tsx
+++ src/_root/pages/LikedPost.tsx
@@ -1,8 +1,27 @@
+import GridPostList from '@/components/shared/GridPostList'
+import Loader from '@/components/shared/Loader'
+import { useGetCurrentUser } from '@/lib/react-query/queriesAndMutations'
 import React from 'react'
 
 const LikedPost = () => {
+  const {data: currentUser} = useGetCurrentUser()
+
+  if(!currentUser){
+    return(
+      <div className='h-full w-full flex-center'>
+        <Loader />
+      </div>
+    )
+  }
   return (
-    <div>LikedPost</div>
+   <>
+   {
+    currentUser.liked.length === 0 && (
+      <p className='text-light-4'>No liked post</p>
+    )
+   }
+   <GridPostList posts={currentUser.liked} showStats={false} />
+   </>
   )
 }
 
