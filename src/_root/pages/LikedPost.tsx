41da443cfe65baf54a8dd05d94c649b400d74b44
saagarbhujel <saagarbhujel22@gmail.com>
2023-11-08 23:53:05 +0545

    clean up

--- src/_root/pages/LikedPost.tsx
+++ src/_root/pages/LikedPost.tsx
@@ -1,7 +1,6 @@
 import GridPostList from '@/components/shared/GridPostList'
 import Loader from '@/components/shared/Loader'
 import { useGetCurrentUser } from '@/lib/react-query/queriesAndMutations'
-import React from 'react'
 
 const LikedPost = () => {
   const {data: currentUser} = useGetCurrentUser()
